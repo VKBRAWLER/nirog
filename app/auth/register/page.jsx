7b1765113139b4862b7183a7f7cebbccc98730be
Vishal Joshi <vishalwebdev0987@gmail.com>
2024-12-07 02:57:48 +0530

    register added'

--- app/auth/register/page.jsx
+++ app/auth/register/page.jsx
@@ -1,20 +1,74 @@
-import 'tailwindcss/tailwind.css';
-import Image from 'next/image';
+"use client";
+import Image from "next/image";
+import { useContext } from "react";
+import BaseContext from "@app/(utils)/context/BaseContext";
 
-const SignInPage = () => {
+const Register = () => {
+  let { baseURL, setAuthToken } = useContext(BaseContext);
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    let name = e.target.fullName.value;
+    let aadhar = e.target.aadharNumber.value;
+    let email = e.target.mail.value;
+    let phone = e.target.phonenumber.value;
+    let password = e.target.password.value;
+    let username = e.target.mail.value;
+    let response = await fetch(`${baseURL}/users/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        name,
+        aadhar,
+        email,
+        phone,
+        password,
+      }),
+    });
+    let data = await response.json();
+    response = await fetch(`${baseURL}/auth/token/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+    data = await response.json();
+    setAuthToken(data);
+    localStorage.setItem("accessToken", JSON.stringify(data));
+  };
   return (
     <main>
       <div className="flex items-center min-h-screen w-full justify-between">
         <div className="lg:w-1/2 lg:block hidden">
-          <img src="/images/animation.gif" alt="Doctor" width="800" height="800" />
+          <img
+            src="/images/animation.gif"
+            alt="Doctor"
+            width="800"
+            height="800"
+          />
         </div>
-        <div className='right min-h-screen flex justify-center items-center bg-gradient-to-r from-white to-[#8ec6f8] lg:h-32 lg:w-1/2'  >
+        <div className="right min-h-screen flex justify-center items-center bg-gradient-to-r from-white to-[#8ec6f8] lg:h-32 lg:w-1/2">
           <div className="flex flex-col w-full max-w-2xl p-6 shadow-lg rounded-lg right h-4/5">
             <div className="mb-6 flex-col justify-center items-center">
-              <div className='flex justify-center items-center'><Image src="/images/nirog.png" alt="Logo" width={250} height={250} /></div>
+              <div className="flex justify-center items-center">
+                <Image
+                  src="/images/nirog.png"
+                  alt="Logo"
+                  width={250}
+                  height={250}
+                />
+              </div>
             </div>
-            <h2 className="text-4xl font-semibold text-center">Create Account</h2>
-            <form className="flex flex-col space-y-6">
+            <h2 className="text-4xl font-semibold text-center">
+              Create Account
+            </h2>
+            <form className="flex flex-col space-y-6" onSubmit={handleSubmit}>
               <input
                 type="text"
                 name="fullName"
@@ -64,4 +118,4 @@ const SignInPage = () => {
   );
 };
 
-export default SignInPage;
+export default Register;
