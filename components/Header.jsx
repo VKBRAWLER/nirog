bd41cd5f195ea895a3260b3db10d03ce721c5b30
Varun Kharkwal <varunkh12345@gmail.com>
2024-12-07 11:58:01 +0530

    Add notification feature to Header component with toggle functionality

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,61 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { IoMdNotifications } from "react-icons/io";
+import { MdOutlineHandshake } from "react-icons/md";
+import { CgDanger } from "react-icons/cg";
+import { useState } from "react";
+
 const Header = () => {
+  const pathname = usePathname().toLowerCase();
+  if (pathname.includes("auth")) return null;
+  const [notifications, setNotifications] = useState(true);
+  const notificationList = [
+    {
+      title: "New Connection",
+      description: "John Doe wants to connect with you",
+    },
+    {
+      title: "Emergency",
+      description: "Jane Doe has sent an SOS",
+    },
+    {
+      title: "New Connection",
+      description: "John Smith wants to connect with you",
+    },
+    {
+      title: "New Connection",
+      description: "Jane Smith wants to connect with you",
+    },
+  ]
   return (
-    <header className='bg-blue-100 h-24'>
-      <img className='h-full m-auto' src="/images/nirog.png" alt="" />
+    <header className='bg-blue-100 h-24 flex justify-between lg:px-5 items-center relative'>
+      <img className='h-full' src="/images/nirog.png" alt="" />
+      <nav className="flex gap-4 lg:gap-8 items-center">
+        <Link className="underline text-blue-900 lg:text-2xl" href={'/dashboard'}>DashBoard</Link>
+        <Link className="underline text-blue-900 lg:text-2xl" href={'/signOut'}>Sign Out</Link>
+        <IoMdNotifications className='text-4xl' onClick={()=>{setNotifications(!notifications);}}/>
+      </nav>
+      {notifications &&
+      <section className="absolute top-28 border-black border-2 right-6 z-20 bg-white shadow-lg w-96 p-4 rounded-lg">
+        <h3 className="text-lg font-semibold mb-2">Notifications</h3>
+        <ul>
+          {notificationList.map((notification, index) => (
+            <li key={index} className="mb-2 border-b-2 border-blue-500 flex items-center gap-2">
+              {notification.title === "Emergency"? <CgDanger className='text-4xl text-red-500'/> :<MdOutlineHandshake className='text-4xl'/>}
+              <div>
+              <h4 className={`font-robo-bold ${notification.title === "Emergency" && "text-red-500"}`}>{notification.title}</h4>
+              <p className="text-sm text-gray-600">{notification.description}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+      }
     </header>
 )
 }
+
+
 
 export default Header;
