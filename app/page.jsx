80dfba730003e41694e918c0015a56ae2361f7a4
Manish Chandra Paneru <manishpaneru2010@gmail.com>
2024-12-07 19:47:01 +0530

    Changed classNameName to className

--- app/page.jsx
+++ app/page.jsx
@@ -36,14 +36,14 @@ export default function Home() {
             <p className="mb-8 leading-relaxed w-1/2">Empowering healthcare with intelligent solutions. Streamline diagnoses, enhance patient care, and redefine medical innovation.
 </p>
 
-            <div classNameName="flex justify-center">
+            <div className="flex justify-center">
               <Link href="/auth/login">
-                <button classNameName="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                   Login
                 </button>
               </Link>
               <Link href="/auth/register">
-                <button classNameName="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
                   Sign up
                 </button>
               </Link>
